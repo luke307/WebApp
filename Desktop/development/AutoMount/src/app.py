f64f75ddacdf6b6b96a5a249d4813f6e7d941fd7
Goebel <DE1119189@targos-gmbh.local>
2020-06-12 12:53:09 +0200

    Add Basic Function Structure

---
+++ Desktop/development/AutoMount/src/app.py
@@ -0,0 +1,26 @@
+import subprocess
+
+
+def shellInput():
+    pass
+
+
+def checkUSB():
+    pass
+
+
+def mount():
+    pass
+
+
+def unmount():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    
+    main()
